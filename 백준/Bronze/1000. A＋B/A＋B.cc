aaf3a865f80e4f3577ebf1b0d2d44062f32d62e5
geneeol <102365022+geneeol@users.noreply.github.com>
2022-05-13 00:45:55 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int    main(void)
+{
+    int    a;
+    int    b;
+    int    sum;
+    
+    std::cin >> a;
+    std::cin >> b;
+    sum = a + b;
+    std::cout << sum;
+    return (0);
+}
