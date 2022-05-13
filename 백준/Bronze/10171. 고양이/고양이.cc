fe966bf6f029d33f8a241bdcfb4c365471a1941e
geneeol <102365022+geneeol@users.noreply.github.com>
2022-05-13 16:01:51 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using	std::cin;
+using	std::cout;
+
+int	main(void)
+{
+	cout << "\\    /\\\n";
+	cout << " )  ( ')\n";
+	cout << "(  /  )\n";
+	cout << " \\(__)|\n";
+	return (0);
+}
