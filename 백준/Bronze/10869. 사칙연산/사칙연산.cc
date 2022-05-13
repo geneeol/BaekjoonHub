4b62c474d2606e92142e59f774b29bfce0ed80e6
geneeol <102365022+geneeol@users.noreply.github.com>
2022-05-13 16:09:06 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int	main(void)
+{
+	int	A;
+	int	B;
+
+	cin >> A >> B;
+	cout << A + B << '\n';
+	cout << A - B << '\n';
+	cout << A * B << '\n';
+	cout << A / B << '\n';
+	cout << A % B << '\n';
+	return (0);
+}
