322b4c74078e74f82e3d904f2b6e1f1ef9ca3a63
geneeol <102365022+geneeol@users.noreply.github.com>
2022-05-14 21:57:51 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using	std::cin;
+using	std::cout;
+
+int	main(void)
+{
+	int	N;
+	int	i;
+	int	j;
+
+	cin >> N;
+	i = 0;
+	while (i < N)
+	{
+		j = 0;
+		while (j < i)
+		{
+			cout << ' ';
+			j++;
+		}
+		j = 0;
+		while (j < N * 2 - i * 2 - 1)
+		{
+			cout << '*';
+			j++;
+		}
+		cout << '\n';
+		i++;
+	}
+
+	i = 1;
+	while (i < N)
+	{
+		j = 0;
+		while (j < N - i - 1)
+		{
+			cout << ' ';
+			j++;
+		}
+		j = 0;
+		while (j < i * 2 + 1)
+		{
+			cout << '*';
+			j++;
+		}
+		cout << '\n';
+		i++;
+	}
+	return (0);
+}
