3342918f77dbc0e03e0c8dd0e39c4993efed4df3
geneeol <102365022+geneeol@users.noreply.github.com>
2022-06-03 22:20:50 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -3,11 +3,67 @@
 
 using namespace std;
 
+class MyQueue
+{
+	int	head;
+	int	tail;
+	int	data[10005];
+public:
+	MyQueue();
+	void push(int element);
+	void pop();
+	int size();
+	bool empty();
+	int front();
+	int back();
+};
+
+MyQueue::MyQueue()
+{
+	head = 0;
+	tail = 0;
+}
+
+void	MyQueue::push(int element)
+{
+	data[tail++] = element;
+}
+
+void	MyQueue::pop()
+{
+	head++;
+}
+
+int	MyQueue::size()
+{
+	return (tail - head);
+}
+
+bool	MyQueue::empty()
+{
+	if (head == tail)
+		return (true);
+	else
+		return (false);
+}
+
+int	MyQueue::front()
+{
+	return (data[head]);
+}
+
+int	MyQueue::back()
+{
+	return (data[tail - 1]);
+}
+
 int	main(void)
 {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	int	N;
 	int	data;
-	queue<int>	qu;
+	MyQueue	qu;
 	string	cmd;
 
 	cin >> N;
