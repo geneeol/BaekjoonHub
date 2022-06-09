03fe07b9b9956779df547bf27efc15adc39de0be
geneeol <102365022+geneeol@users.noreply.github.com>
2022-06-09 22:43:40 +0900

    [Bronze III] Title: 기찍 N, Time: 8 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int	main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int	N;
+
+	cin >> N;
+	while (N)
+	{
+		cout << N << '\n';
+		N--;
+	}
+	return (0);
+}
